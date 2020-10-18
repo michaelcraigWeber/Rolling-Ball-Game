31e77277142a67e040ff41567ea9f20c3b2ce6f9
michaelcraigWeber <47675916+michaelcraigWeber@users.noreply.github.com>
2020-10-18 15:10:17 -0600

    Added Kill zone
    I liked that kill zone idea from the announcement so I stole it

---
+++ Rolling Ball Game/Assets/Scripts/KillZone.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillZone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            UnityEngine.Debug.Log("Player fell out of level area");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
